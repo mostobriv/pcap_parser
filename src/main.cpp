b6b507a1d0ed8366c37992be47657ff2b3d7f6a4
d86leader <d86leader@mail.com>
2019-11-24 01:50:37 +0700

    main.cpp cleanup and fix using unavailable argv

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,17 @@
 
 int main(int argc, char** argv) {
 
-    logger::Logger log ("main", logger::LVL_DEBUG);
-    logger::Logger l2  ("other", logger::LVL_DEBUG);
-
-    log.info() << "starting";
-    l2.info() << "start2";
-    log.debug("info!");
-    l2.debug("info2!");
-    log.debug() << "bug?";
-    l2.debug() << __PRETTY_FUNCTION__;
-
+    if (argc < 2) {
+        logger::logger.error() << "Usage:" << argv[0] << "[*].pcap";
+        return 1;
+    }
 
     PcapLoader foo;
     try {
         foo.parse(argv[1]);
 
     } catch (const std::exception& e) {
-        log.error() << e.what();
+        logger::logger.error() << e.what();
     }
 
     return 0;
